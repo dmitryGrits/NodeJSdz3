af8896806b04efa169d6318bb2bf94deb4a8a6d4
DGRITS <grizay@icloud.com>
2024-04-29 15:46:02 +0300

    server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const express = require('express');
+
+const port = 8080;
+const app = express();
+
+app.use(express.static('static'));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
